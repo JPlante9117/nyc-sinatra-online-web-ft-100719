f4d44086d72fc0b2b3dc354b772492d80ce80bf8
JPlante9117 <jacrplante@gmail.com>
2019-11-19 16:24:45 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,66 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if params[:figure][:title_ids]
+      params[:figure][:title_ids].each {|id| @figure.title_ids << id}
+    end
+    if !params[:title][:name].empty?
+      @title = Title.find_or_create_by(name: params[:title][:name])
+      @figure.titles << @title
+    end
+    if params[:figure][:landmark_ids]
+      params[:figure][:landmark_ids].each {|id| @figure.landmark_ids << id}
+    end
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.find_or_create_by(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+      @figure = Figure.find_by_id(params[:id])
+      erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    @figure.titles = []
+    @figure.landmarks = []
+    if params[:figure][:title_ids]
+      params[:figure][:title_ids].each {|id| @figure.title_ids << id}
+    end
+    if !params[:title][:name].empty?
+      @title = Title.find_or_create_by(name: params[:title][:name])
+      @figure.titles << @title
+    end
+    if params[:figure][:landmark_ids]
+      params[:figure][:landmark_ids].each {|id| @figure.landmark_ids << id}
+    end
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.find_or_create_by(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+  end
 end
